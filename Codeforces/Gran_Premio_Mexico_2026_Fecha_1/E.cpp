931229a9e41d545b35d602e31de38ff926276d3a
Daniel Diaz de Leon <daniel.diaz.deleon@outlook.com>
2026-04-20 11:47:30 -0600

    Gran_Premio/E

--- Codeforces/Gran_Premio_Mexico_2026_Fecha_1/E.cpp
+++ Codeforces/Gran_Premio_Mexico_2026_Fecha_1/E.cpp
@@ -4,10 +4,27 @@ using namespace std;
 #define nl "\n"
 #define ll long long
 
+vector<int> primes;
+vector<int> order; //In here we will store the regular numbers
+int N, Q;
+
+
+void visit(ll current_node_value, int smallest_prime_index) {
+    order.push_back((int)current_node_value);
+
+    //We walk throught the primes
+
+    for (int i = smallest_prime_index; i<primes.size() ;++i) {
+
+        if (current_node_value * primes[i] > N) break; 
+
+        visit(current_node_value * primes[i], i);
+
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-
-    int N, Q;
     cin >> N >> Q;
 
     //You have to use a graph, where each children is a hierarchy and you make children by appending more primes
@@ -44,7 +61,6 @@ for (int i = 2 ; (ll)i*i <= N; ++i) {
         }
     }
 }
-vector<int> primes;
 for (int i = 2; i <=N; ++i) {
     if (is_prime[i]) primes.push_back(i);
 }
@@ -54,8 +70,21 @@ for (int i = 2; i <=N; ++i) {
 // for parent 4, biggest parent in parent 4 = 2x2 is 2, so primes are allowed for p >= 2
 
 
+visit(1, 0);
+vector<int> queries;
+int q;
+for (int i = 0; i < Q; ++i) {
+    cin >> q;
+    queries.push_back(q);
+}
+
+for (int x : queries) {
+    cout << order[x - 1] << nl;
+}
+
 
 
     
-    return 0;
+return 0;
+
 }
